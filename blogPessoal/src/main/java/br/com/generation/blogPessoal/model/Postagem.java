0aaad2181ce07f4b6a2c324f1349fa94a97ca694
ricardo <ricardoliveira50@hotmail.com>
2021-06-29 15:02:22 -0300

    Atualização dos campos Usuario

--- blogPessoal/src/main/java/br/com/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/br/com/generation/blogPessoal/model/Postagem.java
@@ -43,6 +43,11 @@ private Date date = new java.sql.Date(System.currentTimeMillis());
 @JsonIgnoreProperties("postagem")
   private Tema tema;
 
+@ManyToOne
+@JsonIgnoreProperties("postagem")
+private Usuario usuario; 
+
+
 public long getId() {
 	return id;
 }
@@ -81,6 +86,14 @@ public Tema getTema() {
 
 public void setTema(Tema tema) {
 	this.tema = tema;
+}
+
+public Usuario getUsuario() {
+	return usuario;
+}
+
+public void setUsuario(Usuario usuario) {
+	this.usuario = usuario;
 }
 
 
