56a6fd90a6f2fdb6d95cac8f787fecefe4786244
ricardo <ricardoliveira50@hotmail.com>
2021-07-07 22:11:37 -0300

    Blog pessoal V2.3

--- blogPessoal/src/main/java/br/com/generation/blogPessoal/repository/TemaRepository.java
+++ blogPessoal/src/main/java/br/com/generation/blogPessoal/repository/TemaRepository.java
@@ -2,9 +2,11 @@ package br.com.generation.blogPessoal.repository;
 
   
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.generation.blogPessoal.model.Tema;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema,Long> {
 	
 	
